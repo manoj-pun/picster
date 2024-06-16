3a5660a6a3af6bdd05d3ded86ad7646799fb48c7
Manoj Pun <manojpun840@gmail.com>
2024-06-16 15:37:34 +0545

    installed react js and tailwind css

---
+++ frontend/src/Components/LeftSidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from "../Images/picster.png"
+
+const LeftSidebar = () => {
+  return (
+    <div text-red-500>
+      <div>
+        <img src={logo} alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
---
+++ frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import LeftSideBar from '../../Components/LeftSidebar.jsx'
+import RightSideBar from '../../Components/RightSidebar.jsx'
+import Stories from '../../Components/Stories.jsx'
+
+const Home = () => {
+  return (
+     <>
+     <LeftSideBar/>
+     <RightSideBar/>
+     <Stories/>
+    </>
+  )
+}
+
+export default Home
