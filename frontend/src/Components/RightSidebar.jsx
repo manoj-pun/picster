db786894b331757abfe817ce0e5e2f9dae231ca7
Manoj Pun <manojpun840@gmail.com>
2024-06-19 17:27:55 +0545

    started right sidebar and stories section

--- frontend/src/Components/RightSidebar.jsx
+++ frontend/src/Components/RightSidebar.jsx
@@ -1,12 +1,83 @@
 import React from 'react'
-
+import profile from "../Images/images.jpeg"
+import profile1 from "../Images/profile1.jpeg"
+import profile2 from "../Images/profile2.jpeg"
+import profile3 from "../Images/profile3.jpeg"
+import profile4 from "../Images/profile4.jpeg"
+import profile5 from "../Images/profile5.jpeg"
 const RightSidebar = () => {
   return (
-    <div>
-      <div className='bg-red-500 p-4 mr-20'>
-        This is rightsidebar
-
+    <div className=' p-4 mr-20 mt-9'>
+      <div className="flex items-center justify-between mb-4">
+        <div className="flex items-center space-x-4">
+          <img src={profile} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">manojpun</h1>
+            <p className="text-sm text-gray-500">878</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer text-blue-600 font-semibold">Switch</span>
       </div>
+      <div className='flex items-center justify-between mb-3'>
+        <h1 className='text-gray-500 font-semibold'>Suggested for you</h1>
+        <span className="text-sm font-semibold cursor-pointer">See All</span>
+      </div>
+
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex items-center space-x-4">
+          <img src={profile1} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">naruto</h1>
+            <p className="text-sm text-gray-500">Suggested for you</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer">Follow</span>
+      </div>
+
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex items-center space-x-4">
+          <img src={profile2} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">rocklee</h1>
+            <p className="text-sm text-gray-500">Suggested for you</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer">Follow</span>
+      </div>
+
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex items-center space-x-4">
+          <img src={profile3} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">hinata</h1>
+            <p className="text-sm text-gray-500 ">Suggested for you</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer">Follow</span>
+      </div>
+
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex items-center space-x-4">
+          <img src={profile4} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">sakura</h1>
+            <p className="text-sm text-gray-500">Suggested for you</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer">Follow</span>
+      </div>
+
+      <div className="flex items-center justify-between mb-3">
+        <div className="flex items-center space-x-4">
+          <img src={profile5} alt="" className="w-10 h-10 rounded-full object-cover cursor-pointer" />
+          <div>
+            <h1 className=" font-semibold cursor-pointer">sakura1</h1>
+            <p className="text-sm text-gray-500">Suggested for you</p>
+          </div>
+        </div>
+        <span className=" cursor-pointer">Follow</span>
+      </div>
+
     </div>
   )
 }
--- frontend/src/Components/Stories.jsx
+++ frontend/src/Components/Stories.jsx
@@ -1,12 +1,37 @@
 import React from 'react'
+import profile1 from "../Images/profile1.jpeg"
+import profile2 from "../Images/profile2.jpeg"
+import profile3 from "../Images/profile3.jpeg"
+import profile4 from "../Images/profile4.jpeg"
+import profile5 from "../Images/profile5.jpeg"
 
 const Stories = () => {
   return (
-    <div>
-      <div className="bg-pink-200 h-[80px] ml-4 mb-6">
-        This is stories
+    <div className= 'ml-14 mb-6 mt-6'>
+      <div className='flex p-2 space-x-5 '>
+        <div className="flex flex-col items-center">
+          <img src={profile1} alt="" className='w-[70px] h-[70px] rounded-full object-cover mb-2 cursor-pointer'/>
+          <span className="text-sm cursor-pointer">naruto</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={profile2} alt="" className='w-[70px] h-[70px] rounded-full object-cover mb-2 cursor-pointer'/>
+          <span className="text-sm cursor-pointer">rocklee</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={profile3} alt="" className='w-[70px] h-[70px] rounded-full object-cover mb-2 cursor-pointer'/>
+          <span className="text-sm cursor-pointer">hinata</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={profile4} alt="" className='w-[70px] h-[70px] rounded-full object-cover mb-2 cursor-pointer'/>
+          <span className="text-sm cursor-pointer">sakura</span>
+        </div>
+        <div className="flex flex-col items-center">
+          <img src={profile5} alt="" className='w-[70px] h-[70px] rounded-full object-cover mb-2 cursor-pointer'/>
+          <span className="text-sm cursor-pointer">sakura1</span>
+        </div>
       </div>
     </div>
+
   )
 }
 
